0bdb7960e855a25913d121f8c5a9f275ef9d1229
Tiago Vieira <tiagovmvieira@hotmail.com>
2022-04-22 17:23:38 +0100

    refator

--- app/main.py
+++ app/main.py
@@ -6,36 +6,36 @@ from fastapi.params import Body
 from typing import List
 from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
-from .database import engine, get_db
+from .database import engine
 from . import models, schemas, utils
 from .routers import post, user, auth
 
 import psycopg2
 import time
 
-models.Base.metadata.create_all(bind = engine)
+def main():
+    models.Base.metadata.create_all(bind = engine)
 
-app = FastAPI() #fastapi instantiation
+    app = FastAPI() #fastapi instantiation
 
-while True:
-    try:
-        conn = psycopg2.connect(host = 'localhost',
-                                database = 'Social_Media_FASTAPI',
-                                user = 'tiagovieira',
-                                password = 'password123',
-                                cursor_factory = RealDictCursor)
-        cursor = conn.cursor()
-        print('Database Connection was sucessfull!')
-        break
-    except Exception as error:
-        print('Connection to database failed')
-        print('Error:', error)
-        time.sleep(3)
+    while True:
+        try:
+            conn = psycopg2.connect(host = 'localhost',
+                                    database = 'Social_Media_FASTAPI',
+                                    user = 'tiagovieira',
+                                    password = 'password123',
+                                    cursor_factory = RealDictCursor)
+            cursor = conn.cursor()
+            print('Database Connection was sucessfull!')
+            break
+        except Exception as error:
+            print('Connection to database failed')
+            print('Error:', error)
+            time.sleep(3)
 
-app.include_router(post.router)
-app.include_router(user.router)
-app.include_router(auth.router)
+    app.include_router(post.router)
+    app.include_router(user.router)
+    app.include_router(auth.router)
 
-@app.get('/')
-def root() -> dict:
-    return {'message': 'Hello World'}
+if __name__ == '__main__':
+    main()
