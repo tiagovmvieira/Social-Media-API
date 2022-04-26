3997b5f62511f10e104ad756e45c656a2665498b
Tiago Vieira <tiagovmvieira@hotmail.com>
2022-04-26 18:21:36 +0100

    refactor

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Post(Base):
     content = Column(String, nullable = False)
     published = Column(Boolean, server_default = 'TRUE', nullable = False)
     created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default = text('now()'))
-    owner_id = Column(Integer, ForeignKey('users.id', ondelete = 'CASCADE'), nullable = False)
+    owner_id = Column(Integer, ForeignKey('users.user_id', ondelete = 'CASCADE'), nullable = False)
 
 class User(Base):
     __tablename__ = 'users'
--- app/routers/post.py
+++ app/routers/post.py
@@ -19,7 +19,7 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 
 @router.post('/', status_code = status.HTTP_201_CREATED, response_model = schemas.PostResponse)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user))-> models.Post:
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id = current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -33,18 +33,27 @@ def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oaut
     if (not post):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
                             detail = 'post with id: {} was not found'.format(id))
+    
+    if (post.owner_id != user_id.user.id):
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN,
+                            detail = 'Not authorized to perform requested action')
 
     return post
 
 @router.delete('/{id}', status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user))-> Response:
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_queried = post_query.first()
 
-    if (post.first() is None):
+    if (post_queried is None):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
                             detail = 'post with id: {} does not exist'.format(id))
 
-    post.delete(synchronize_session = False)
+    if (post_queried.owner_id != current_user.id):
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN,
+                            detail = 'Not authorized to perform requested action')
+
+    post_queried.delete(synchronize_session = False)
     db.commit()
 
     return Response(status_code = status.HTTP_204_NO_CONTENT) #required to send the 204 status code
@@ -58,6 +67,10 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
                             detail = 'post with id: {} does not exist'.format(id))
     
+    if (post.owner_id != current_user.id):
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN,
+                            detail = 'Not authorized to perform requested action')
+
     post_query.update(post.dict(), synchronize_session = False)
     db.commit()
     db.refresh(post_query.first())
