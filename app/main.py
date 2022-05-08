e5115ff2a914811050392003e653248d5ff936f5
Tiago Vieira <tiagovmvieira@hotmail.com>
2022-05-08 23:27:55 +0100

    add wip files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 
 #import modules
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 
 from .database import engine
@@ -14,19 +14,27 @@ app = FastAPI() #fastapi instantiation
 
 origins = ['*']
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = origins,
-    allow_credential = True,
-    allow_methods = ['*'],
-    allow_headers = ['*'],
-)
+#app.add_middleware(
+#    CORSMiddleware,
+#        allow_origins = origins,
+#    allow_credential = True,
+#    allow_methods = ['*'],
+#    allow_headers = ['*'],
+#)
+
+#app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=origins,
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+#)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
-@app.get('/')
+@app.get('/', status_code = status.HTTP_200_OK)
 def root():
     return {'message': 'Social_Media_FASTAPI'}
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,15 @@
 from fastapi.testclient import TestClient
-from app.main import app 
+from app.main import app
 
-print(type(app))
 client = TestClient(app)
 
 def test_root():
     res = client.get('/')
-    print(res)
+    print(res.json().get('message'))
+    assert res.json().get('message') == 'Social_Media_FASTAPI'
+    assert res.status_code == 200
+
+def test_create_user():
+    res = client.post("/users/", json = {"email": "hello123@gmail.com", "password": "password123"})
+    print(res.json())
+    assert res.status_code == 201
